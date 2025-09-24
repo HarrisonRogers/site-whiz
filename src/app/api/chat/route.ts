bc6ad471732ad1353ab5f99470179e5319a7c0e7
harrison rogers <harrison.rogers05@gmail.com>
2025-09-24 21:10:17 +1200

    chore: update functions

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,10 +1,10 @@
 import { openai } from '@ai-sdk/openai';
-import { streamText } from 'ai';
+import { streamText, UIMessage } from 'ai';
 
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
     model: openai('gpt-5-mini'),
